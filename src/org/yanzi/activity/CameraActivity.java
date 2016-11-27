503f21486140d4fdf02face6928884b02c8426d0
inksci <inksci@qq.com>
2016-11-27 22:37:56 +0800

    reenter after click home is ok. CAMERA_FACING_FRONT

---
+++ src/org/yanzi/activity/CameraActivity.java
@@ -0,0 +1,132 @@
+package org.yanzi.activity;
+
+import org.yanzi.camera.CameraInterface;
+import org.yanzi.camera.CameraInterface.CamOpenOverCallback;
+import org.yanzi.camera.preview.CameraSurfaceView;
+import org.yanzi.playcamera.R;
+import org.yanzi.util.DisplayUtil;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.SurfaceHolder;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class CameraActivity extends Activity implements CamOpenOverCallback {
+	private static final String TAG = "yanzi";
+	CameraSurfaceView surfaceView = null;
+	ImageButton shutterBtn;
+	float previewRate = -1f;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+	
+    @Override  
+    protected void onStart() {  
+        super.onStart();  
+        Log.v(TAG, "protected void onStart()");  
+        Thread openThread = new Thread(){
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				CameraInterface.getInstance().doOpenCamera(CameraActivity.this);
+			}
+		};
+		Toast.makeText(getApplicationContext(), "默认Toast样式", Toast.LENGTH_SHORT).show();
+		openThread.start();
+		setContentView(R.layout.activity_camera);
+		initUI();
+		initViewParams();
+		
+		shutterBtn.setOnClickListener(new BtnListeners());
+    }  
+ 
+    @Override  
+    protected void onPause() {  
+        super.onPause();  
+        Log.v(TAG, "protected void onPause()");  
+    }  
+ 
+    @Override  
+    protected void onResume() {  
+        super.onResume();  
+        Log.v(TAG, "protected void onResume()");  
+    }  
+ 
+    @Override  
+    protected void onStop() {  
+        super.onStop();  
+        Log.v(TAG, "protected void onStop()");  
+    }  
+ 
+    @Override  
+    protected void onDestroy() {  
+        super.onDestroy();  
+        Log.v(TAG, "protected void onDestroy()");  
+    }  
+ 
+    @Override  
+    protected void onRestart() {  
+        super.onRestart();  
+        Log.v(TAG, "protected void onRestart()");  
+    }  
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.camera, menu);
+		return true;
+	}
+
+	private void initUI(){
+		surfaceView = (CameraSurfaceView)findViewById(R.id.camera_surfaceview);
+		shutterBtn = (ImageButton)findViewById(R.id.btn_shutter);
+	}
+	private void initViewParams(){
+		LayoutParams params = surfaceView.getLayoutParams();
+		Point p = DisplayUtil.getScreenMetrics(this);
+		params.width = p.x;
+		params.height = p.y;
+		previewRate = DisplayUtil.getScreenRate(this); //默认全屏的比例预览
+		surfaceView.setLayoutParams(params);
+
+		//手动设置拍照ImageButton的大小为120dip×120dip,原图片大小是64×64
+		LayoutParams p2 = shutterBtn.getLayoutParams();
+		p2.width = DisplayUtil.dip2px(this, 80);
+		p2.height = DisplayUtil.dip2px(this, 80);;		
+		shutterBtn.setLayoutParams(p2);	
+
+	}
+
+	@Override
+	public void cameraHasOpened() {
+		// TODO Auto-generated method stub
+		SurfaceHolder holder = surfaceView.getSurfaceHolder();
+		CameraInterface.getInstance().doStartPreview(holder, previewRate);
+	}
+	private class BtnListeners implements OnClickListener{
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			switch(v.getId()){
+			case R.id.btn_shutter:
+				CameraInterface.getInstance().doTakePicture();
+				break;
+			default:break;
+			}
+		}
+
+	}
+
+}
