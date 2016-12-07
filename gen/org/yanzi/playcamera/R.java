634287a3226ed68715d1b0d46425db9e3d933527
inksci <inksci@qq.com>
2016-12-07 08:40:25 +0800

    Done: display_inker

--- gen/org/yanzi/playcamera/R.java
+++ gen/org/yanzi/playcamera/R.java
@@ -27,15 +27,17 @@ public final class R {
         public static final int btn_shutter_background=0x7f020003;
         public static final int camera=0x7f020004;
         public static final int camera7=0x7f020005;
-        public static final int ic_launcher_icon=0x7f020006;
-        public static final int inker1=0x7f020007;
-        public static final int inker11=0x7f020008;
-        public static final int inker6=0x7f020009;
-        public static final int inker7=0x7f02000a;
-        public static final int introduce02=0x7f02000b;
-        public static final int logo01=0x7f02000c;
-        public static final int testit=0x7f02000d;
-        public static final int welcom=0x7f02000e;
+        public static final int display_inker=0x7f020006;
+        public static final int display_inker2=0x7f020007;
+        public static final int ic_launcher_icon=0x7f020008;
+        public static final int inker1=0x7f020009;
+        public static final int inker11=0x7f02000a;
+        public static final int inker6=0x7f02000b;
+        public static final int inker7=0x7f02000c;
+        public static final int introduce02=0x7f02000d;
+        public static final int logo01=0x7f02000e;
+        public static final int testit=0x7f02000f;
+        public static final int welcom=0x7f020010;
     }
     public static final class id {
         public static final int action_settings=0x7f080009;
