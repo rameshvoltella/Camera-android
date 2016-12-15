824996864a729caaee972335d7f3fcfac2c47fa9
inksci <inksci@qq.com>
2016-12-16 07:12:00 +0800

    finish: delete useless more

--- gen/org/yanzi/playcamera/R.java
+++ gen/org/yanzi/playcamera/R.java
@@ -23,43 +23,33 @@ public final class R {
     public static final class drawable {
         public static final int btn_camera_all=0x7f020000;
         public static final int btn_camera_all_click=0x7f020001;
-        public static final int btn_shutter_background=0x7f020002;
-        public static final int camera7=0x7f020003;
-        public static final int display_inker=0x7f020004;
-        public static final int display_inker2=0x7f020005;
-        public static final int exit=0x7f020006;
-        public static final int ic_launcher_icon=0x7f020007;
-        public static final int inker_play=0x7f020008;
-        public static final int logo01=0x7f020009;
-        public static final int make_not=0x7f02000a;
-        public static final int make_yes=0x7f02000b;
-        public static final int no_make=0x7f02000c;
-        public static final int try_it=0x7f02000d;
-        public static final int welcom=0x7f02000e;
+        public static final int camera7=0x7f020002;
+        public static final int display_inker=0x7f020003;
+        public static final int exit=0x7f020004;
+        public static final int ic_launcher_icon=0x7f020005;
+        public static final int inker_play=0x7f020006;
+        public static final int logo01=0x7f020007;
+        public static final int make_not=0x7f020008;
+        public static final int make_yes=0x7f020009;
+        public static final int no_make=0x7f02000a;
+        public static final int try_it=0x7f02000b;
+        public static final int welcom=0x7f02000c;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000b;
+        public static final int action_settings=0x7f080007;
         public static final int btn_shutter=0x7f080001;
-        public static final int button1=0x7f080009;
-        public static final int button2=0x7f08000a;
         public static final int camera_surfaceview=0x7f080000;
-        public static final int editText1=0x7f080005;
         public static final int imageView1=0x7f080003;
         public static final int imageView2=0x7f080002;
-        public static final int one=0x7f080008;
         public static final int textView1=0x7f080004;
-        public static final int textView2=0x7f080006;
-        public static final int textView3=0x7f080007;
+        public static final int textView2=0x7f080005;
+        public static final int textView3=0x7f080006;
     }
     public static final class layout {
         public static final int activity_camera=0x7f030000;
-        public static final int button=0x7f030001;
-        public static final int introduce_02=0x7f030002;
-        public static final int logo_01=0x7f030003;
-        public static final int over_05=0x7f030004;
-        public static final int result_04_02=0x7f030005;
-        public static final int result_04_03=0x7f030006;
-        public static final int view_02=0x7f030007;
+        public static final int introduce_02=0x7f030001;
+        public static final int logo_01=0x7f030002;
+        public static final int result_04_02=0x7f030003;
     }
     public static final class menu {
         public static final int camera=0x7f070000;
