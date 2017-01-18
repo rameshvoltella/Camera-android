5be6d01534d1f7e411b51420260f5dfab61dbeb9
inksci <inksci@outlook.com>
2017-01-18 20:40:18 +0800

    succeed: with voice recorder

--- src/org/yanzi/activity/CameraActivity.java
+++ src/org/yanzi/activity/CameraActivity.java
@@ -15,10 +15,14 @@ import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 
 import android.graphics.BitmapFactory;
+import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 
 import android.os.Bundle;
@@ -39,6 +43,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import android.view.View.OnClickListener;
 
@@ -130,6 +136,25 @@ import java.util.Map;
 import android.util.Base64;
 
 public class CameraActivity extends Activity implements CamOpenOverCallback {
+	//////////////////////////////////////////
+	    // 音频获取源
+		private int audioSource = MediaRecorder.AudioSource.MIC;
+		// 设置音频采样率，44100是目前的标准，但是某些设备仍然支持22050，16000，11025
+		private static int sampleRateInHz = 8000;
+		// 设置音频的录制的声道CHANNEL_IN_STEREO为双声道，CHANNEL_CONFIGURATION_MONO为单声道
+		private static int channelConfig = AudioFormat.CHANNEL_IN_STEREO;
+		// 音频数据格式:PCM 16位每个样本。保证设备支持。PCM 8位每个样本。不一定能得到设备支持。
+		private static int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+		// 缓冲区字节大小
+		private int bufferSizeInBytes = 0;
+		private Button Start;
+		private Button Stop;
+		private AudioRecord audioRecord;
+		private boolean isRecord = false;// 设置正在录制的状态
+		
+		//AudioName裸音频数据文件
+		private static final String AudioName = Environment.getExternalStorageDirectory().getAbsolutePath()+"/inker-audio.pcm";
+	////////////// END
 	boolean new_visit;
 	ImageView master_control;
 	
@@ -766,8 +791,144 @@ public class CameraActivity extends Activity implements CamOpenOverCallback {
     protected void onStart() {
         super.onStart();
         
-        chat_page();
+        
+        getWindow().setFormat(PixelFormat.TRANSLUCENT);// 让界面横屏
+		requestWindowFeature(Window.FEATURE_NO_TITLE);// 去掉界面标题
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		// 重新设置界面大小
+		setContentView(R.layout.recorder);
+		init();
+        
     }
+    
+    
+    
+    
+    
+    
+    
+    
+	private void init() {
+		Start = (Button) this.findViewById(R.id.start);
+		Stop = (Button) this.findViewById(R.id.stop);
+		Start.setOnClickListener(new TestAudioListener());
+		Stop.setOnClickListener(new TestAudioListener());
+		creatAudioRecord();
+		
+		Button btn1=(Button)this.findViewById(R.id.button1);
+		btn1.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+            	chat_page();
+            	
+            }});
+		
+	}
+
+	private void creatAudioRecord() {
+		// 获得缓冲区字节大小
+		bufferSizeInBytes = AudioRecord.getMinBufferSize(sampleRateInHz,
+				channelConfig, audioFormat);
+		// 创建AudioRecord对象
+		audioRecord = new AudioRecord(audioSource, sampleRateInHz,
+				channelConfig, audioFormat, bufferSizeInBytes);
+	}
+
+	class TestAudioListener implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			if (v == Start) {
+				startRecord();
+			}
+			if (v == Stop) {
+				stopRecord();
+			}
+
+		}
+
+	}
+
+	private void startRecord() {
+		audioRecord.startRecording();
+		// 让录制状态为true
+		isRecord = true;
+		// 开启音频文件写入线程
+		new Thread(new AudioRecordThread()).start();
+	}
+
+	private void stopRecord() {
+		close();
+	}
+
+	private void close() {
+		if (audioRecord != null) {
+			System.out.println("stopRecord");
+			isRecord = false;//停止文件写入
+			audioRecord.stop();
+			audioRecord.release();//释放资源
+			audioRecord = null;
+		}
+	}
+
+	class AudioRecordThread implements Runnable {
+		@Override
+		public void run() {
+			writeDateTOFile();//往文件中写入裸数据
+			//copyWaveFile(AudioName, NewAudioName);//给裸数据加上头文件
+		}
+	}
+
+	/**
+	 * 这里将数据写入文件，但是并不能播放，因为AudioRecord获得的音频是原始的裸音频，
+	 * 如果需要播放就必须加入一些格式或者编码的头信息。但是这样的好处就是你可以对音频的 裸数据进行处理，比如你要做一个爱说话的TOM
+	 * 猫在这里就进行音频的处理，然后重新封装 所以说这样得到的音频比较容易做一些音频的处理。
+	 */
+	private void writeDateTOFile() {
+		// new一个byte数组用来存一些字节数据，大小为缓冲区大小
+		byte[] audiodata = new byte[bufferSizeInBytes];
+		FileOutputStream fos = null;
+		int readsize = 0;
+		try {
+			File file = new File(AudioName);
+			if (file.exists()) {
+				file.delete();
+			}
+			fos = new FileOutputStream(file);// 建立一个可存取字节的文件
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		while (isRecord == true) {
+			readsize = audioRecord.read(audiodata, 0, bufferSizeInBytes);
+			if (AudioRecord.ERROR_INVALID_OPERATION != readsize) {
+				try {
+					fos.write(audiodata);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		try {
+			fos.close();// 关闭写入流
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     protected void chat_page(){
     	setContentView(R.layout.chat);
     	
