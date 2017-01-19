325bb96695b1ec12c272308852b0dbc1fb6f0a91
inksci <inksci@outlook.com>
2017-01-19 20:54:57 +0800

    Done: with TTS

--- src/org/yanzi/activity/CameraActivity.java
+++ src/org/yanzi/activity/CameraActivity.java
@@ -106,6 +106,11 @@ import org.yanzi.playcamera.R;
 
 import org.yanzi.util.DisplayUtil;
 
+import com.baidu.tts.auth.AuthInfo;
+import com.baidu.tts.client.SpeechError;
+import com.baidu.tts.client.SpeechSynthesizer;
+import com.baidu.tts.client.SpeechSynthesizerListener;
+import com.baidu.tts.client.TtsMode;
 import com.inksci.function.InkFunction;
 
 import java.io.BufferedReader;
@@ -135,8 +140,21 @@ import java.util.Map;
 
 import android.util.Base64;
 
-public class CameraActivity extends Activity implements CamOpenOverCallback {
-	//////////////////////////////////////////
+public class CameraActivity extends Activity implements CamOpenOverCallback, SpeechSynthesizerListener {
+	///////////////////
+	private SpeechSynthesizer mSpeechSynthesizer;
+    private String mSampleDirPath;
+    private static final String SAMPLE_DIR_NAME = "baiduTTS";
+    private static final String SPEECH_FEMALE_MODEL_NAME = "bd_etts_speech_female.dat";
+    private static final String SPEECH_MALE_MODEL_NAME = "bd_etts_speech_male.dat";
+    private static final String TEXT_MODEL_NAME = "bd_etts_text.dat";
+    private static final String LICENSE_FILE_NAME = "temp_license_2017-01-00";
+    private static final String ENGLISH_SPEECH_FEMALE_MODEL_NAME = "bd_etts_speech_female_en.dat";
+    private static final String ENGLISH_SPEECH_MALE_MODEL_NAME = "bd_etts_speech_male_en.dat";
+    private static final String ENGLISH_TEXT_MODEL_NAME = "bd_etts_text_en.dat";
+	/////// TTS Finish
+	
+		//////////////////////////////////////////
 	    // 音频获取源
 		private int audioSource = MediaRecorder.AudioSource.MIC;
 		// 设置音频采样率，44100是目前的标准，但是某些设备仍然支持22050，16000，11025
@@ -154,7 +172,7 @@ public class CameraActivity extends Activity implements CamOpenOverCallback {
 		
 		//AudioName裸音频数据文件
 		private static final String AudioName = Environment.getExternalStorageDirectory().getAbsolutePath()+"/inker-audio.pcm";
-	////////////// END
+		////////////// END
 	boolean new_visit;
 	ImageView master_control;
 	
@@ -791,6 +809,10 @@ public class CameraActivity extends Activity implements CamOpenOverCallback {
     protected void onStart() {
         super.onStart();
         
+        ///////////// TTS Begin
+        initialEnv();
+        startTTS();
+        // TTS Finish
         
         getWindow().setFormat(PixelFormat.TRANSLUCENT);// 让界面横屏
 		requestWindowFeature(Window.FEATURE_NO_TITLE);// 去掉界面标题
@@ -1409,6 +1431,184 @@ public class CameraActivity extends Activity implements CamOpenOverCallback {
         // TODO Auto-generated method stub
         get_bitmap = true;
     }
+    /////////////////////////////////////////// TTS START
+    // 初始化语音合成客户端并启动
+    private void startTTS() {
+/*查看Key×
+App ID: 9150869
 
+API Key: yzGVe4oXOdflbEICtnYbhe7A
+
+Secret Key: c9644b9b942a565dbf633d2581cb89f2*/    	
+        // 获取语音合成对象实例
+        mSpeechSynthesizer = SpeechSynthesizer.getInstance();
+        // 设置context
+        mSpeechSynthesizer.setContext(this);
+        // 设置语音合成状态监听器
+        mSpeechSynthesizer.setSpeechSynthesizerListener(this);
+        // 设置在线语音合成授权，需要填入从百度语音官网申请的api_key和secret_key
+        mSpeechSynthesizer.setApiKey("yzGVe4oXOdflbEICtnYbhe7A", "c9644b9b942a565dbf633d2581cb89f2");
+        // 设置离线语音合成授权，需要填入从百度语音官网申请的app_id
+        mSpeechSynthesizer.setAppId("9150869");
+        /*
+        // 设置语音合成文本模型文件
+        mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_TEXT_MODEL_FILE, "your_txt_file_path");
+        // 设置语音合成声音模型文件
+        mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_SPEECH_MODEL_FILE, "your_speech_file_path");
+        // 设置语音合成声音授权文件
+        mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_LICENCE_FILE, "your_licence_path");
+        */
+        
+        this.mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_TEXT_MODEL_FILE, mSampleDirPath + "/"
+                + TEXT_MODEL_NAME);
+        // 声学模型文件路径 (离线引擎使用)
+        this.mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_SPEECH_MODEL_FILE, mSampleDirPath + "/"
+                + SPEECH_FEMALE_MODEL_NAME);
+        // 本地授权文件路径,如未设置将使用默认路径.设置临时授权文件路径，LICENCE_FILE_NAME请替换成临时授权文件的实际路径，仅在使用临时license文件时需要进行设置，如果在[应用管理]中开通了正式离线授权，不需要设置该参数，建议将该行代码删除（离线引擎）
+        // 如果合成结果出现临时授权文件将要到期的提示，说明使用了临时授权文件，请删除临时授权即可。
+        this.mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_LICENCE_FILE, mSampleDirPath + "/"
+                + LICENSE_FILE_NAME);
+        // 发音人（在线引擎），可用参数为0,1,2,3。。。（服务器端会动态增加，各值含义参考文档，以文档说明为准。0--普通女声，1--普通男声，2--特别男声，3--情感男声。。。）
+        this.mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_SPEAKER, "0");
+        // 设置Mix模式的合成策略
+        this.mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_MIX_MODE, SpeechSynthesizer.MIX_MODE_DEFAULT);
+        
+        // 获取语音合成授权信息
+        AuthInfo authInfo = this.mSpeechSynthesizer.auth(TtsMode.MIX);
+
+        if (authInfo.isSuccess()) {
+            //toPrint("auth success");
+            mSpeechSynthesizer.initTts(TtsMode.MIX);
+            //mSpeechSynthesizer.speak("百度语音合成示例程序正在运行");
+            
+        } else {
+            String errorMsg = authInfo.getTtsError().getDetailMessage();
+            //toPrint("auth failed errorMsg=" + errorMsg);
+        }
+
+        // 初始化tts
+        mSpeechSynthesizer.initTts(TtsMode.MIX);
+        // 加载离线英文资源（提供离线英文合成功能）
+        int result =
+                mSpeechSynthesizer.loadEnglishModel(mSampleDirPath + "/" + ENGLISH_TEXT_MODEL_NAME, mSampleDirPath
+                        + "/" + ENGLISH_SPEECH_FEMALE_MODEL_NAME);
+        //toPrint("loadEnglishModel result=" + result);
+
+        //打印引擎信息和model基本信息
+        //printEngineInfo();
+        
+        int tt = this.mSpeechSynthesizer.speak("你好,我是小墨，您的个人机器人");
+    }    
+    private void initialEnv() {
+        if (mSampleDirPath == null) {
+            String sdcardPath = Environment.getExternalStorageDirectory().toString();
+            mSampleDirPath = sdcardPath + "/" + SAMPLE_DIR_NAME;
+        }
+        makeDir(mSampleDirPath);
+        copyFromAssetsToSdcard(false, SPEECH_FEMALE_MODEL_NAME, mSampleDirPath + "/" + SPEECH_FEMALE_MODEL_NAME);
+        copyFromAssetsToSdcard(false, SPEECH_MALE_MODEL_NAME, mSampleDirPath + "/" + SPEECH_MALE_MODEL_NAME);
+        copyFromAssetsToSdcard(false, TEXT_MODEL_NAME, mSampleDirPath + "/" + TEXT_MODEL_NAME);
+        copyFromAssetsToSdcard(false, LICENSE_FILE_NAME, mSampleDirPath + "/" + LICENSE_FILE_NAME);
+        copyFromAssetsToSdcard(false, "english/" + ENGLISH_SPEECH_FEMALE_MODEL_NAME, mSampleDirPath + "/"
+                + ENGLISH_SPEECH_FEMALE_MODEL_NAME);
+        copyFromAssetsToSdcard(false, "english/" + ENGLISH_SPEECH_MALE_MODEL_NAME, mSampleDirPath + "/"
+                + ENGLISH_SPEECH_MALE_MODEL_NAME);
+        copyFromAssetsToSdcard(false, "english/" + ENGLISH_TEXT_MODEL_NAME, mSampleDirPath + "/"
+                + ENGLISH_TEXT_MODEL_NAME);
+    }
+    private void makeDir(String dirPath) {
+        File file = new File(dirPath);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+    }   
+    private void copyFromAssetsToSdcard(boolean isCover, String source, String dest) {
+        File file = new File(dest);
+        if (isCover || (!isCover && !file.exists())) {
+            InputStream is = null;
+            FileOutputStream fos = null;
+            try {
+                is = getResources().getAssets().open(source);
+                String path = dest;
+                fos = new FileOutputStream(path);
+                byte[] buffer = new byte[1024];
+                int size = 0;
+                while ((size = is.read(buffer, 0, 1024)) >= 0) {
+                    fos.write(buffer, 0, size);
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (fos != null) {
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    if (is != null) {
+                        is.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+  
+
+
+
+	@Override
+	public void onError(String arg0, SpeechError arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onSpeechFinish(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onSpeechProgressChanged(String arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onSpeechStart(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onSynthesizeDataArrived(String arg0, byte[] arg1, int arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onSynthesizeFinish(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onSynthesizeStart(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+    ///////////////////// TTS END
 
 }
