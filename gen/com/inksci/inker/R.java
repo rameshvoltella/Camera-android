155bc95ea7f0292883c7feb02b08f558fa3e8fa9
inksci <inksci@outlook.com>
2017-01-24 20:37:35 +0800

    save

--- gen/com/inksci/inker/R.java
+++ gen/com/inksci/inker/R.java
@@ -10,6 +10,14 @@ package com.inksci.inker;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int content_color=0x7f040003;
+        /**  控件按下时显示的颜色 
+         */
+        public static final int list_backgroud_color=0x7f040001;
+        public static final int title_color=0x7f040002;
+        public static final int white=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,49 +25,88 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int camera7=0x7f020000;
         public static final int camera_ic_launcher_icon=0x7f020001;
-        public static final int display_inker=0x7f020002;
-        public static final int exit=0x7f020003;
-        public static final int ic_launcher_icon=0x7f020004;
-        public static final int inker_play=0x7f020005;
-        public static final int logo01=0x7f020006;
-        public static final int make_not=0x7f020007;
-        public static final int make_yes=0x7f020008;
-        public static final int no_make=0x7f020009;
-        public static final int try_it=0x7f02000a;
+        public static final int cancel=0x7f020002;
+        public static final int cancel_button=0x7f020003;
+        public static final int cancel_p=0x7f020004;
+        public static final int common_icon_searchbox_voice_pressed=0x7f020005;
+        public static final int common_listitem_singleline_normal=0x7f020006;
+        public static final int delete_selector=0x7f020007;
+        public static final int display_inker=0x7f020008;
+        public static final int exit=0x7f020009;
+        public static final int ic_launcher=0x7f02000a;
+        public static final int ic_launcher_icon=0x7f02000b;
+        public static final int inker_play=0x7f02000c;
+        /**  画面背景色 
+         */
+        public static final int layout_backgroud=0x7f02001a;
+        /**  分割线背景色 
+         */
+        public static final int line_background=0x7f02001b;
+        public static final int logo01=0x7f02000d;
+        public static final int maikefeng=0x7f02000e;
+        public static final int make_not=0x7f02000f;
+        public static final int make_yes=0x7f020010;
+        public static final int no_make=0x7f020011;
+        public static final int ok=0x7f020012;
+        public static final int ok_button=0x7f020013;
+        public static final int ok_d=0x7f020014;
+        public static final int ok_p=0x7f020015;
+        public static final int search_clear_normal=0x7f020016;
+        public static final int search_clear_pressed=0x7f020017;
+        public static final int superman=0x7f020018;
+        public static final int try_it=0x7f020019;
     }
     public static final class id {
-        public static final int TextView01=0x7f080007;
-        public static final int action_settings=0x7f08000a;
-        public static final int btn_shutter=0x7f080001;
-        public static final int camera_surfaceview=0x7f080000;
-        public static final int imageView1=0x7f080003;
-        public static final int imageView2=0x7f080002;
-        public static final int start=0x7f080005;
-        public static final int stop=0x7f080006;
-        public static final int textView1=0x7f080004;
-        public static final int textView2=0x7f080008;
-        public static final int textView3=0x7f080009;
+        public static final int RelativeLayout1=0x7f09000d;
+        public static final int TextView01=0x7f09000a;
+        public static final int action_settings=0x7f090012;
+        public static final int btn_shutter=0x7f090001;
+        public static final int button1=0x7f090004;
+        public static final int camera_surfaceview=0x7f090000;
+        public static final int cancel=0x7f090010;
+        public static final int content=0x7f09000f;
+        public static final int editText=0x7f090002;
+        public static final int im2=0x7f090003;
+        public static final int imageView1=0x7f090006;
+        public static final int imageView2=0x7f090005;
+        public static final int ok=0x7f090011;
+        public static final int start=0x7f090008;
+        public static final int stop=0x7f090009;
+        public static final int textView1=0x7f090007;
+        public static final int textView2=0x7f09000b;
+        public static final int textView3=0x7f09000c;
+        public static final int title=0x7f09000e;
     }
     public static final class layout {
         public static final int activity_camera=0x7f030000;
-        public static final int introduce_02=0x7f030001;
-        public static final int logo_01=0x7f030002;
-        public static final int recorder=0x7f030003;
-        public static final int result_04_02=0x7f030004;
+        public static final int activity_main=0x7f030001;
+        public static final int introduce_02=0x7f030002;
+        public static final int logo_01=0x7f030003;
+        public static final int recorder=0x7f030004;
+        public static final int result_04_02=0x7f030005;
+        public static final int superman_alertdialog=0x7f030006;
     }
     public static final class menu {
-        public static final int camera=0x7f070000;
+        public static final int camera=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_id=0x7f060003;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int superman_cancel=0x7f060005;
+        public static final int superman_content=0x7f060007;
+        /**  提示安装语音+对话框 
+         */
+        public static final int superman_ok=0x7f060004;
+        public static final int superman_title=0x7f060006;
+        public static final int text_begin=0x7f060008;
     }
     public static final class style {
         /** 
@@ -83,10 +130,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int dialog=0x7f070002;
     }
 }
